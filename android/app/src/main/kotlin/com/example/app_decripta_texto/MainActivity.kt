768950d7c0a9d4acbf337a0aaefc44f20f2dafbd
Pedro Lucas Fernandes <plff201100@gmail.com>
2023-10-01 03:45:20 -0300

    init: first commit

---
+++ android/app/src/main/kotlin/com/example/app_decripta_texto/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_decripta_texto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:app_decripta_texto/pages/cifradecesar.dart';
+import 'package:app_decripta_texto/pages/cifradevigenere.dart';
+import 'package:app_decripta_texto/pages/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main () => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: "/homepage",
+      routes: {
+        "/cifradecesar" : (context) => CesarPage(),
+        "/cifradevigenere" : (context) => VigenerePage(),
+      },
+      home: HomePage(),
+    );
+  }
+}
