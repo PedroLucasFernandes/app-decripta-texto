4638ff3a8d6b4a207f53f2f9f9dfa37203a3319b
Pedro Lucas Fernandes <plff201100@gmail.com>
2023-10-01 04:03:27 -0300

    feat: add homepage and routes

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -4,6 +4,56 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(
+          "Seja muito bem-vindo(a)!",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(40),
+        child: ListView(
+          children: <Widget>[
+            Text("Selecione a cifra que você quer decriptar:"),
+            SizedBox(height: 30,),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.blue),
+                shape: MaterialStatePropertyAll(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              onPressed : () {
+                Navigator.pushNamed(context, '/cifradecesar');
+              },
+              child : Text("Cifra de César")
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.blue),
+                shape: MaterialStatePropertyAll(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              onPressed : () {
+                Navigator.pushNamed(context, '/cifradevigenere');
+              },
+              child : Text("Cifra de Vigenère"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
